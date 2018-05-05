f25be5f91f2c9b4edd45f15fa5753d48e2c8065f
miltonmb <miltonmb@unitec.edu>
2018-05-05 13:39:23 -0600

    listo el conversor

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,34 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public Fa:number;
+  public Response;
+  public holder:string = "°C";
+  public holder2:string = "°F";
   constructor(public navCtrl: NavController) {
 
+  }
+
+  conversionC(){
+    if(this.holder == "°C"){
+      console.log("entro aqui tambien");
+      this.Response = (this.Fa*(9/5))+32;
+    }else{
+      console.log("entro aqui");
+      this.Response = (this.Fa-32)*(5/9);
+    }
+  }
+
+  swap(){
+    if(this.holder == "°C"){
+      this.holder = "°F";
+      this.holder2 = "°C";
+      this.Response = "";
+    }else{
+      this.holder = "°C";
+      this.holder2 = "°F";
+      this.Response = "";
+    }
   }
 
 }
